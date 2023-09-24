28776085af9fa13be2726ade34d958c5a2aa94f0
apex <gloryofapex@gmail.com>
2023-09-24 08:22:33 +0800

    fix ui

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
 const targetUrl = 'https://cms.cnpg.co.nz'
+
 const getPostsList = () => {
+    // fetch('http://localhost:3000/rest/posts')
     let yearOptionsElement = document.querySelector('#filter2')
     let homeEventsElement = document.querySelector('#events')
     let events = []
     let yearsObj = {}
     fetch('\n' +
-       `${targetUrl}/api/articles?pagination[limit]=99999&pagination[start]=0&pagination[withCount]=true&populate=image,category,author,seo&locale=en`)
+        `${targetUrl}/api/articles?pagination[limit]=99999&pagination[start]=0&pagination[withCount]=true&populate=image,category,author,seo&locale=en`)
         .then(response => response.json())
         .then(data => {
-            for (const item of data.data) {
-                const year = item.attributes.year;
+                        const postsDiv = document.getElementById('posts');
+                        data.data.forEach(post => {
+                            const postElement = document.createElement('div');
+                            const titleElement = document.createElement('h2');
+                            const contentElement = document.createElement('p');
+                        for (const item of data.data) {
+                            const year = item.attributes.year;
 
-                if (!yearsObj[year]) {
-                    yearsObj[year] = [];
-                }
+                            if (!yearsObj[year]) {
+                                yearsObj[year] = [];
+                            }
 
-                yearsObj[year].push(item);
-            }
-            let select_html = []
-            let masonry_html = []
-            Object.keys(yearsObj).sort((a, b) => b - a).forEach((item, index) => {
-                select_html.push(`<li><a id="select-${item}" href="#${item}" data-option-value=".${item}" title="${item}">${item}</a></li>`)
-                yearsObj[item].sort((a, b) => {
-                    return new Date(b.attributes.createdAt) - new Date(a.attributes.createdAt)
-                }).forEach(_item => {
-                    masonry_html.push(`<li class="col-sm-6 col-lg-4 ${item}">
-                                <div class="pop-course">
-                                    <div class="course-thumb">
-                                        <img class="event-img" src="${targetUrl + _item.attributes.image.data.attributes.url}" alt="">
-                                        <span>${new Date(_item.attributes.createdAt).toLocaleString()}</span>
-                                        <a href="blog.html?id=${_item.attributes.slug}" target="_blank" class="butn">Learn more</a>
-                                    </div>
-                                    <div class="course-meta">
-                                        <div class="course-author">
-                                            <img src="assets/images/author.png" alt="">
-                                            <span>${_item.attributes.title}</span>
-                                        </div>
-                                        <h2>${_item.attributes.ckeditor_content.slice(0, 96)}...</h2>
-                                    </div>
-                                </div>
-                            </li>`)
-                })
+                            yearsObj[year].push(item);
+                        }
+                        let select_html = []
+                        let masonry_html = []
+                            console.log(yearsObj)
+                        Object.keys(yearsObj).sort((a, b) => b - a).forEach((item, index) => {
+                            select_html.push(`<li><a id="select-${item}" href="#${item}" data-option-value=".${item}" title="${item}">${item}</a></li>`)
+                            yearsObj[item].sort((a, b) => {
+                                return new Date(b.attributes.createdAt) - new Date(a.attributes.createdAt)
+                            }).forEach(_item => {
+                                masonry_html.push(`<li class="col-sm-6 col-lg-4 ${item}">
+                                            <div class="pop-course">
+                                                <div class="course-thumb">
+                                                    <img class="event-img" src="${targetUrl + _item.attributes.image.data.attributes.url}" alt="">
+                                                    <span>${new Date(_item.attributes.createdAt).toLocaleString()}</span>
+                                                    <a href="blog.html?id=${_item.attributes.slug}" target="_blank" class="butn">Learn more</a>
+                                                </div>
+                                                <div class="course-meta">
+                                                    <div class="course-author">
+                                                        <img src="assets/images/author.png" alt="">
+                                                        <span>${_item.attributes.title}</span>
+                                                    </div>
+                                                    <h2>${_item.attributes.ckeditor_content.slice(0, 96)}...</h2>
+                                                </div>
+                                            </div>
+                                        </li>`)
+                            })
 
-            })
+                        })
 
-            yearOptionsElement.innerHTML = select_html.join('')
+                            titleElement.textContent = post.title;
+                            contentElement.textContent = post.content;
+                        yearOptionsElement.innerHTML = select_html.join('')
 
-            if (location.href.indexOf('events') > -1) {
-                homeEventsElement.innerHTML = masonry_html.join('')
-            } else {
-                homeEventsElement.innerHTML = masonry_html.slice(0, 6).join('')
-            }
+                            postElement.appendChild(titleElement);
+                            postElement.appendChild(contentElement);
+                        if (location.href.indexOf('events') > -1) {
+                            homeEventsElement.innerHTML = masonry_html.join('')
+                        } else {
+                            homeEventsElement.innerHTML = masonry_html.slice(0, 6).join('')
+                        }
 
+                            postsDiv.appendChild(postElement);
+                        });
             if (location.search.indexOf('year') > -1) {
                 let year = location.search.split('=')[1]
                 setTimeout(() => {
@@ -107,7 +121,7 @@ function initCalendar(events) {
         views: {
             yearGrid: {
                 type: 'dayGrid',
-                duration: { years: 1 },
+                duration: {years: 1},
                 buttonText: 'year'
             }
         },
